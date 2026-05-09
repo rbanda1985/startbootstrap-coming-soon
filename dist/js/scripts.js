c2811058eb4e84b75c81649535f9f8c8c523c1e1
rbanda1985 <robertbanda85@gmail.com>
2026-05-09 12:58:49 -0500

    packages and site changes

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,6 +1,6 @@
 /*!
 * Start Bootstrap - Coming Soon v6.0.7 (https://startbootstrap.com/theme/coming-soon)
-* Copyright 2013-2023 Start Bootstrap
+* Copyright 2013-2026 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-coming-soon/blob/master/LICENSE)
 */
 // This file is intentionally blank
